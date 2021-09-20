e8ba920dea658ea8d60841eff11a038b458a9524
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-09-20 21:27:51 +0530

    added csurf middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import compression from "compression";
+import csurf from "csurf";
 
 import httpLogger from "./middlewares/httpLogger";
 import apiRouter from "./routes";
@@ -15,6 +16,7 @@ const app: express.Express = express();
     app.use(express.json());
     app.use(compression());
     app.use(cors());
+    app.use(csurf());
     app.use(helmet());
     app.disable("x-powered-by");
     app.set("trust proxy", 1);
