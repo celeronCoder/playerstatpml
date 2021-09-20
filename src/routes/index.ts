807bf6c4fe5a35a9740c18c1568483a7c0edd144
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-09-20 22:49:29 +0530

    added base api response with available routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,33 @@
-import express from "express";
-import { topScorerController, topAssistController } from "../controllers";
+import express, { Request, Response } from "express";
+import { topGoalsController, topAssistController } from "../controllers";
 
 const apiRouter: express.Router = express.Router();
 
-apiRouter.get("/topScorers", topScorerController);
+apiRouter.get("/topGoals", topGoalsController);
 apiRouter.get("/topAssist", topAssistController);
+
+namespace API {
+    interface Route {
+        path: string;
+        method: "GET" | "POST" | "PUT" | "DELETE";
+    }
+
+    const routes: Route[] = [];
+
+    for (let i = 0; i < apiRouter.stack.length; i++) {
+        routes.push({
+            path: apiRouter.stack[i].route.path,
+            method: apiRouter.stack[i].route.stack[0].method.toUpperCase(),
+        });
+    }
+
+    export function controller(req: Request, res: Response) {
+        return res.status(200).json({
+            routes,
+        });
+    }
+}
+
+apiRouter.get("", API.controller);
 
 export default apiRouter;
